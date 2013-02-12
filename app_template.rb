87cab5543e9a7bb22a5df3ed28805ddba8913b46
Andreas Eriksson <andreas@codered.se>
2013-02-12 21:39:03 +0100

    added wget-commant

--- app_template.rb
+++ app_template.rb
@@ -27,7 +27,7 @@ if yes? "Do you want to generate a root controller?"
   remove_file "public/index.html"
 end
 
-run "wget https://raw.github.com/andreaseriksson/apptemplate/master/_form.html.erb -O lib/templates/erb/scaffold/_form.html.erb"
+run "wget https://raw.github.com/andreaseriksson/apptemplate/master/_form.html.erb -O"
 
 git :init
 append_file ".gitignore", "config/database.yml"
