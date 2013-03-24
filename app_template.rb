3366344e3fc5e06ba359495b1b290abcbe5aa7ae
Andreas Eriksson <andreas@codered.se>
2013-03-24 14:13:44 +0100

    Updates

--- app_template.rb
+++ app_template.rb
@@ -1,18 +1,27 @@
 
-gem 'bootstrap-sass', '~> 2.2.2.0'
+gem 'bootstrap-sass', '~> 2.3.1.0'
 gem 'haml'
 gem 'simple_form'
 gem 'bourbon'
 gem 'bcrypt-ruby', '~> 3.0.0'
 
 gem "rspec-rails", group: [:test, :development]
+gem_group :test do
+  gem "factory_girl_rails"
+  gem "capybara"
+  gem "guard-rspec"
+  gem "faker"
+  gem "poltergeist"
+end
 
-gem "factory_girl_rails", group: [:test]
-gem "capybara", group: [:test]
-gem "guard-rspec", group: [:test]
+gem_group :development do
+  gem "better_errors"
+  gem "binding_of_caller"
+end
 
-gem "better_errors", group: [:development]
-gem "binding_of_caller", group: [:development]
+gem_group :production do
+  gem "pg"
+end
 
 run "bundle install"
 generate "rspec:install"
@@ -27,9 +36,38 @@ if yes? "Do you want to generate a root controller?"
   remove_file "public/index.html"
 end
 
-run "curl https://raw.github.com/andreaseriksson/apptemplate/master/_form.html.erb > lib/templates/erb/scaffold/_form.html.erb"
+#run "curl https://raw.github.com/andreaseriksson/apptemplate/master/_form.html.erb > lib/templates/erb/scaffold/_form.html.erb"
+
+
+application do
+  # ./config/application.rb
+  config.generators.stylesheets = false
+  config.generators.javascripts = false
+  config.generators.helper      = false
+  
+  
+  config.generators do |g|
+    g.test_framework :rspec,
+      :fixtures => true,
+      :view_specs => false,
+      :helper_specs => false,
+      :routing_specs => false,
+      :controller_specs => true,
+      :request_specs => true
+    g.fixture_replacement :factory_girl, :dir => "spec/factories"
+  end
+end
+
+append_to_file 'config/application.rb' do
+  CONFIG = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+  CONFIG.merge! CONFIG.fetch(Rails.env, {})
+  CONFIG.symbolize_keys!
+end
+
 
 git :init
 append_file ".gitignore", "config/database.yml"
+append_file ".gitignore", "config/application.yml"
 run "cp config/database.yml config/example_database.yml"
+create_file "config/application.yml"
 git add: ".", commit: "-m 'initial commit'"
