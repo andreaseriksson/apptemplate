e4cd5358972263176aba5c6e7736b62f59f484c4
Andreas Eriksson <andreas@codered.se>
2014-03-30 11:59:52 +0200

    Updated for rails 4

--- app_template.rb
+++ app_template.rb
@@ -1,9 +1,11 @@
 
-gem 'bootstrap-sass', '~> 2.3.1.0'
-gem 'haml'
+gem 'bootstrap-sass', '~> 3.1.1'
+gem 'font-awesome-rails'
 gem 'simple_form'
 gem 'bourbon'
-gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'bcrypt-ruby', '~> 3.1.2'
+#gem 'ancestry'
+#gem 'acts_as_list'
 
 gem "rspec-rails", group: [:test, :development]
 gem_group :test do
@@ -28,13 +30,6 @@ generate "rspec:install"
 generate "simple_form:install --bootstrap"
 
 run "guard init rspec"
-
-if yes? "Do you want to generate a root controller?"
-  name = ask("What should it be called?").underscore
-  generate :controller, "#{name} index"
-  route "root to: '#{name}\#index'"
-  remove_file "public/index.html"
-end
 
 application do
   %Q{
@@ -86,4 +81,82 @@ host: ''
 }
 end
 run "cp config/application.yml config/application.example.yml"
+
+
+if yes? "Do you want to generate a root controller?[yes/no]"
+  name = ask("What should it be called?").underscore
+  generate :controller, "#{name} index"
+  route "root to: '#{name}\#index'"
+end
+
+
+if yes? "Do you want to generate a admin area?[yes/no]"
+  generate :resource, "user email:string:index password_digest"
+  run "rake db:migrate && rake db:test:prepare"
+  generate :controller, "sessions index"
+
+  inject_into_class "app/models/user.rb", User do
+    "  hvalidates_uniqueness_of :email\n"
+    "  attr_accessible :email, :password, :password_confirmation\n"
+    "  has_secure_password\n"
+  end
+  
+end
+
+
+# Main view
+remove_file "app/view/application.html.erb"
+create_file "app/view/application.html.erb"
+append_to_file "app/view/application.html.erb" do
+%Q{
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title></title>
+    <%= stylesheet_link_tag "application", media: "all" %>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+      <%= yield %>
+    </div><!-- /.container -->
+
+    <%= javascript_include_tag "application" %>
+    <%= csrf_meta_tags %>
+  </body>
+</html>
+}
+
+
 git add: ".", commit: "-m 'initial commit'"
